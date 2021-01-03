c51efe31412fd31cb64f27e1b57527dfd1b2577d
Marcely Santello <marcely@sbsoftware.com.br>
2021-01-03 20:32:28 -0300

    OrderStatus enum

--- src/main/java/com/mackenzie/course/config/TestConfig.java
+++ src/main/java/com/mackenzie/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.mackenzie.course.entities.Order;
 import com.mackenzie.course.entities.User;
+import com.mackenzie.course.entities.enums.OrderStatus;
 import com.mackenzie.course.repositories.OrderRepository;
 import com.mackenzie.course.repositories.UserRepository;
 
@@ -28,10 +29,10 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria", "maria@gmail.com", "55555555", "123456");
 		User u2 = new User(null, "João", "joão.costa@gmail.com", "5454545", "abcdef");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		Order o4 = new Order(null, Instant.parse("2019-01-20T00:30:22Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
+		Order o4 = new Order(null, Instant.parse("2019-01-20T00:30:22Z"), OrderStatus.SHIPPED, u2);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
