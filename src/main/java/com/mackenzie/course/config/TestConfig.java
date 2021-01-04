d380235601f871ec365d091c66e94bcbe0809a7e
Marcely Santello <marcely@sbsoftware.com.br>
2021-01-04 00:48:06 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/mackenzie/course/config/TestConfig.java
+++ src/main/java/com/mackenzie/course/config/TestConfig.java
@@ -48,17 +48,31 @@ public class TestConfig implements CommandLineRunner{
 		Category c2 = new Category(null, "Books");
 		Category c3 = new Category(null, "Clothes and fashion");
 		Category c4 = new Category(null, "Pets");
+		Category c5 = new Category(null, "Computers");
 		
 		Product p1 = new Product(null, "Laptop dell", "Laptop black 15HD 256 GB", 3756.00f, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0f, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0f, "");
-		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0f, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99f, "");
+		Product p4 = new Product(null, "Uma longa jornada", "Uma longa jornada. Autor: Nicholas Sparks. Editora: Arqueiro", 35.0f, "");
+		Product p5 = new Product(null, "Ração premium", "Ração premium sabor churrasco para cachorros até 10kg", 60.99f, "");
+		Product p6 = new Product(null, "Ração premium", "Ração premium sabor churrasco para cachorros acima de 10kg", 78.99f, "");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
-		categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6));
+		
+		p1.getCategories().add(c1);
+		p1.getCategories().add(c5);
+		p2.getCategories().add(c1);
+		p3.getCategories().add(c1);
+		p3.getCategories().add(c5);
+		p4.getCategories().add(c2);
+		p5.getCategories().add(c4);
+		p6.getCategories().add(c4);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6));
+		
 		
 	}	
 	
