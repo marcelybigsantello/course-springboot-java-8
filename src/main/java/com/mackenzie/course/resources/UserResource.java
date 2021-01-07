02f19ff9056208fe6519a70d843397bcb90eae59
Marcely Santello <marcely@sbsoftware.com.br>
2021-01-06 23:05:25 -0300

    User update

--- src/main/java/com/mackenzie/course/resources/UserResource.java
+++ src/main/java/com/mackenzie/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,13 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 		
 	} 
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
--- src/main/java/com/mackenzie/course/services/UserService.java
+++ src/main/java/com/mackenzie/course/services/UserService.java
@@ -28,6 +28,18 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
+	public User update(Integer id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());;
+	}
+
 	public void delete(Integer id) {
 		repository.deleteById(id);
 	}
