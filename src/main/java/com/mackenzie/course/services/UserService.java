59c80679e7cc2f045351c8b72995d0916eeaed34
Marcely Santello <marcely@sbsoftware.com.br>
2021-01-08 00:12:38 -0300

    Exception handling - delete

--- src/main/java/com/mackenzie/course/services/UserService.java
+++ src/main/java/com/mackenzie/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mackenzie.course.entities.User;
 import com.mackenzie.course.repositories.UserRepository;
+import com.mackenzie.course.services.exceptions.DatabaseException;
 import com.mackenzie.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -42,6 +45,15 @@ public class UserService {
 	}
 
 	public void delete(Integer id) {
-		repository.deleteById(id);
+		try {			
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 }
